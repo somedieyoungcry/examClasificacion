e8184c3ce33d05ed5b66ac8e16c0e288fd60fd17
somedieyoungcry <jfuentess004@alumno.uaemex.mx>
2022-11-21 16:54:56 -0600

    Regresion Logistica fin

--- prueba1/prueba3/urls.py
+++ prueba1/prueba3/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path(r'', views.index, name='index'),
     path('post_list/', views.muestra_datos, name='post_list'),
     path('algoritmo_knn/', views.algoritmo_knn, name='algoritmo_knn'),
-    path('algoritmo_cbi/', views.algoritmo_cbi, name='algoritmo_cbi')
+    path('algoritmo_cbi/', views.algoritmo_cbi, name='algoritmo_cbi'),
+    path('algoritmo_rl/', views.regresionLog, name='algoritmo_rl'),
+    path('equivalente/', views.interpretar, name='equivalente'),
 ]
--- prueba1/prueba3/views.py
+++ prueba1/prueba3/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from .models import post2
+from django.http import HttpResponse
 import math
-from numpy import mean, var
+from numpy import mean, var, exp
 import numpy as np
+
+#import numpy as np
 import random
 # Create your views here.
 
@@ -147,3 +150,50 @@ def calcular_varianza(arr, is_sample=False):
     else:
         variance = sum__sqr_diff/(len(arr)-1)
     return variance
+
+def regresionLog(request):
+    return render(request, 'app1/algoritmo_rl.html')
+
+def interpretar(request):
+    if request.GET["x1"].isdigit() and request.GET["x2"].isdigit():
+        x1 = int(request.GET["x1"])
+        x2 = int(request.GET["x2"])
+        datos = post2.objects.all()
+        b = calcConstante(datos)
+        resultado  = valorReferente(datos, x1, x2, b)
+        return render(request, 'app1/equivalente.html', {'consulta': resultado} )
+    else:
+        mensaje = "Te falto llenar o llenaste incorrectamente, recuerda que deben ser valores numericos"
+    return HttpResponse(mensaje)
+
+def valorReferente(datos, x1, x2, b):
+    a1 = 0
+    a2 = 0
+    caracter = ''
+    for i in datos:
+        a1 = i.x1
+        caracter = i.x2
+        a2 = i.x3
+        break
+    salida = 1/(1 + np.exp(-(a1*x1 + a2*x2 + b)))
+    if salida > 0.5:
+        respuesta = f'El caracter obtenido es: {caracter}'
+    else:
+        respuesta = f'No hay caracter que haya podido encontrar: {caracter}'
+    return respuesta
+
+def calcConstante(datos):
+    x = []
+    y = []
+    xCuadrada = 0
+    xy = 0
+    for i in datos:
+        xCuadrada = xCuadrada + i.x1**2
+        xy = xy + i.x1 * i .x3
+        x.append(i.x1)
+        y.append(i.x3)
+    xSum = sum(x)
+    ySum = sum(y)
+    constante = (xCuadrada*ySum - xy*xSum)/(datos.count()*xCuadrada-xSum**2)
+    return constante
+    
